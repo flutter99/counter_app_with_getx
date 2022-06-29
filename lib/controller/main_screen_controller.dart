88eab02f433924485bfa3fff324792dd05c1bcd3
flutter99 <82260050+flutter99@users.noreply.github.com>
2022-06-29 12:11:40 +0500

    all files added

---
+++ lib/controller/main_screen_controller.dart
@@ -0,0 +1,16 @@
+import 'package:get/get.dart';
+
+class MainScreenController extends GetxController {
+
+  RxInt value = 0.obs;
+
+
+  void add() {
+    value.value++;
+  }
+
+  void subtract() {
+    value.value--;
+  }
+
+}
